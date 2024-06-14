6aebdffe96f45fed1e12e19a14e5099fde1f2c2d
DBaku <149767371+DBaku@users.noreply.github.com>
2024-06-14 13:21:22 +0200

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Pokédex</title>
     <link href="{{ mix('css/pokedex.css') }}" rel="stylesheet">
+    <style>
+.container {
+    border: 2px solid black; 
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: red;
+}
+
+.card {
+
+    margin-top: 20px;
+}
+
+.card-header {
+    text-align: center;
+}
+
+.card-body {
+    display: flex;
+    align-items: center;
+}
+
+.img-fluid {
+    max-width: 100%;
+    height: auto;
+}
+
+.input-group {
+    margin-bottom: 20px;
+}
+    </style>
 </head>
 <body>
     <div id="app">
